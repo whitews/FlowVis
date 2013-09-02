fe929506e0af81d8d92cee057238a33f332c512a
whitews <whitews@gmail.com>
2013-09-02 14:56:39 -0400

    made a simpler scatterplot example for the "SimpleScatterplot" and renamed the animated one

--- SimpleScatterplot/js/main.js
+++ SimpleScatterplot/js/main.js
@@ -13,10 +13,7 @@ var x_range;              // used for "auto-range" for chosen x category
 var y_range;              // used for "auto-range" for chosen x category
 var x_scale;              // function to convert x data to svg pixels
 var y_scale;              // function to convert y data to svg pixels
-var circles;              // svg circle elements for the data points
-var flow_data;            // flow data array
 var parameter_list = [];  // flow data column names
-var transition_ms = 2000; // default time in milliseconds for the plot animation
 
 var svg = d3.select("#scatterplot")
     .append("svg")
@@ -26,9 +23,6 @@ var svg = d3.select("#scatterplot")
 var plot_area = svg.append("g")
     .attr("id", "plot-area")
     .attr("transform", "translate(" + margin.left + ", " + (height - margin.bottom) + ")");
-
-var brush = d3.svg.brush()
-    .on("brush", brushed);
 
 var x_axis = plot_area.append("g")
     .attr("class", "axis");
@@ -52,75 +46,26 @@ var tooltip = d3.select("body")
     .style("z-index", "100")
     .style("visibility", "hidden");
 
-// load the CSV and save the data
-d3.csv("../986d883313.csv", function(error, data) {
-    flow_data = data;
-
-    // Grab our column names while we're here
-    for (var key in flow_data[0]) {
+// load the CSV data
+d3.csv("986d883313.csv", function(error, data) {
+    // Grab our column names
+    for (var key in data[0]) {
         if (key != "category") {
             parameter_list.push(key);
         }
     }
 
-    // Populate x & y select options, both get all the columns
-    for (var i in parameter_list) {
-        $("#xList_select")
-            .append($('<option></option>')
-                .attr("value", parameter_list[i])
-                .text(parameter_list[i]));
-        $("#yList_select")
-            .append($('<option></option>')
-                .attr("value", parameter_list[i])
-                .text(parameter_list[i]));
-    }
-
-    // Initialize our plot points (circle elements)
-    //   - note the initial positions are at (0, 0)
-    circles = plot_area.selectAll("circle").data(flow_data);
-
-    circles.enter()
-        .append("circle")
-            .attr("cx", 0)
-            .attr("cy", 0)
-            .attr("r", radius)
-            .on("mouseover", function(d) {  // setup our mouseover
-                tooltip.style("visibility", "visible");
-                tooltip.text(d[x_cat] + ", " + d[y_cat]);
-            })
-            .on("mousemove", function(){return tooltip.style("top",
-                (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10) + "px");})
-            .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
-
-    /*
-     *    Note:
-     *    The mouse events get a little wonky b/c
-     *    the d3 brush consumes them for drawing.
-     *    There is no z-index in SVG, the layer is
-     *    based on document order. Even if you put
-     *    the brush before all the circles, the
-     *    mouseover works, but it breaks the brush.
-     *    Maybe there's a way to capture the mouse
-     *    events and "route" them to the respective
-     *    element???
-     */
-
-    // Might as well show the user an initial plot!
-    plot();
-});
-
-function plot() {
-    // Get current x & y categories from the select option
-    x_cat = $("#xList_select").val();
-    y_cat = $("#yList_select").val();
+    // Choose random x & y categories
+    x_cat = parameter_list[Math.floor(Math.random()*parameter_list.length)];
+    y_cat = parameter_list[Math.floor(Math.random()*parameter_list.length)];
 
     // Update the axes' labels with the new categories
     x_label.text(x_cat);
     y_label.text(y_cat);
 
     // Get the new ranges to calculate the axes' scaling
-    x_range = d3.extent(flow_data, function(d) { return parseInt(d[x_cat]);});
-    y_range = d3.extent(flow_data, function(d) { return parseInt(d[y_cat]);});
+    x_range = d3.extent(data, function(d) { return parseInt(d[x_cat]);});
+    y_range = d3.extent(data, function(d) { return parseInt(d[y_cat]);});
 
     // Pad ranges by 5% so the points aren't right on the edge
     x_range[0] = Math.round(x_range[0]*.90);
@@ -136,34 +81,17 @@ function plot() {
     x_axis.call(d3.svg.axis().scale(x_scale).orient("bottom"));
     y_axis.call(d3.svg.axis().scale(y_scale).orient("left"));
 
-    // Clear any existing brushes, and update the brush scale
-    // Need to re-append our updated brush to the plot as well
-    d3.selectAll("g.brush").remove();
-    brush
-        .x(x_scale)
-        .y(y_scale);
-    plot_area.append("g")
-        .attr("class", "brush")
-        .call(brush);
-
-    // Update the cx & cy attributes of our circles
-    circles.transition().duration(2000)
-        .attr("cx", function (d) { return x_scale(d[x_cat]); })
-        .attr("cy", function (d) { return y_scale(d[y_cat]); });
-}
-
-function brushed() {
-    var e = brush.extent();
-
-    // Breaking down the extent for easier reading :)
-    var x_min = e[0][0];
-    var x_max = e[1][0];
-    var y_min = e[0][1];
-    var y_max = e[1][1];
-
-    // Highlight selected circles
-    svg.selectAll("circle").classed("selected", function(d) {
-        return d[x_cat] > x_min && d[x_cat] < x_max
-            && d[y_cat] > y_min && d[y_cat] < y_max;
-    });
-}
+    // Plot the data points as SVG circle elements
+    plot_area.selectAll("circle").data(data).enter()
+        .append("circle")
+            .attr("cx", function (d) { return x_scale(d[x_cat]); })
+            .attr("cy", function (d) { return y_scale(d[y_cat]); })
+            .attr("r", radius)
+            .on("mouseover", function(d) {  // setup our mouseover
+                tooltip.style("visibility", "visible");
+                tooltip.text(d[x_cat] + ", " + d[y_cat]);
+            })
+            .on("mousemove", function(){return tooltip.style("top",
+                (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10) + "px");})
+            .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+});
